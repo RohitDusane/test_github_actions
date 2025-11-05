ce6766ea263ff0de1fffeb7e2a79a56f4698c466
RohitDusane <stat.data247@gmail.com>
2025-11-05 13:33:03 +0530

    eval models

--- test/model_test.py
+++ test/model_test.py
@@ -1,5 +1,5 @@
 import pytest
-from model.model import load_data, preprocess_data, train_model, evaluate_model
+from model.model import load_data, preprocess_data, train_model, eval_model
 
 def test_data_loading():
     X, y = load_data()
@@ -10,5 +10,5 @@ def test_model_accuracy():
     X, y = load_data()
     X_train, X_test, y_train, y_test = preprocess_data(X, y)
     model = train_model(X_train, y_train)
-    accuracy = evaluate_model(model, X_test, y_test)
+    accuracy = eval_model(model, X_test, y_test)
     assert accuracy > 0.9  # Ensure the model achieves above 90% accuracy (reasonable threshold for Iris dataset)
